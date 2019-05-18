8811e637264446f29daa2c7332fad6e068729ab4
@amrit1207! <jainamrit233@gmail.com>
2019-05-18 17:49:08 +0530

    updations are done

--- employee/views.py
+++ employee/views.py
@@ -8,11 +8,11 @@ def emp(request):
     if request.method == "POST":
         form = EmployeeForm(request.POST)
         if form.is_valid():
-            try:
-                form.save()
-                return redirect('/show')
-            except:
-                pass
+                try:
+                    form.save()
+                    return redirect('/show')
+                except:
+                    pass
     else:
             form = EmployeeForm()
     return render(request,'index.html',{'form':form})
